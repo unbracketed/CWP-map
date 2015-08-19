85749f9f89a23e08c3b421c408e0fba53421f4ce
Brian Luft <brian@lincolnloop.com>
2015-08-19 10:48:09 -0700

    Render the map as a vector layer

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 import ol from 'openlayers'
 
+var projection = ol.proj.get('EPSG:3857');
+
+var vector = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    url: 'loop.kml',
+    format: new ol.format.KML()
+  })
+});
+
 const map = new ol.Map({
   target: 'map',
   layers: [
     new ol.layer.Tile({
-      source: new ol.source.MapQuest({layer: 'sat'})
-    })
+      source: new ol.source.MapQuest({layer: 'sat'}),
+    }),
+    vector
   ],
   view: new ol.View({
-    center: ol.proj.transform([-117.7197, 34.1100], 'EPSG:4326', 'EPSG:3857'),
-    zoom: 4
+    center: ol.proj.transform([-117.706799, 34.139004], 'EPSG:4326', 'EPSG:3857'),
+    zoom: 14,
+    projection
   })
 })
